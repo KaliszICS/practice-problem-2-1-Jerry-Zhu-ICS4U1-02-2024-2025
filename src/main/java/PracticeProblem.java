7d9e65205e51b80eae6182788dfc92dff8b78f8f
Jerry-Zhu-ICS4U1-02-2024-2025 <jerry.zhu2@student.tdsb.on.ca>
2025-03-18 21:59:37 +0000

    finish practice problems

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -3,7 +3,36 @@ public class PracticeProblem {
 	public static void main(String args[]) {
 
 	}
-
+public static int find(int[] arr, int num){
+	int index = -1;
+	for(int i = 0; i < arr.length; i++){
+		if (arr[i] == num){
+			return i;
+		}
+	}
+	return index;
+}
 	
+public static int findLast(String[] arr, String phrase){
+	int index = -1;
+	for(int i = 0; i < arr.length; i++){
+		if(arr[i].equals(phrase)){
+			index = i;
+		}
+	}
+	return index;
+}
 
+public static int findSecond(char[] arr, char c){
+	int index = -1;
+	int counter = 0;
+	for(int i = 0; i < arr.length; i++){
+
+		if(arr[i] == c && counter < 2){
+			index = i;
+			counter++;
+		}
+	}
+	return index;
+}
 }
